ea490994367b6e173a089d291da6ca39567a7b04
Erik Bergbom <erik.bergbom@sogeti.com>
2022-12-07 23:40:30 +0100

    moving code for day 7

--- 07/Program.cs
+++ 07/Program.cs
@@ -1,42 +1,13 @@
-﻿using System.Collections.Generic;
-using System.IO;
-Dictionary<string, int> fileStructure = new Dictionary<string, int>();
+﻿Dictionary<string, int> fileStructure = new Dictionary<string, int>();
 int totalSum = 0;
+// Read and traverse the file
 using (StreamReader sr = new StreamReader(@".\input.txt")) 
 {
-    string line = "";
-    // Iterating the file
     while (sr.Peek() >= 0)
     {
         readDirectory(sr, "root");
     }
 }
-
-foreach(KeyValuePair<string, int> entry in fileStructure){
-    if(entry.Value < 100000)
-        totalSum +=entry.Value;
-}
-System.Console.WriteLine($"Answer Part 1: {totalSum.ToString()}");
-
-int totalSpace = 70000000;
-int requiredSpace = 30000000;
-
-int lowestFolder = fileStructure["root//"];
-int remainingSpace = totalSpace - lowestFolder;
-foreach(KeyValuePair<string, int> entry in fileStructure){
-    if(remainingSpace + entry.Value >= requiredSpace)
-    {
-        if(entry.Value < lowestFolder)
-        {
-            lowestFolder = entry.Value;
-            //Console.WriteLine($"New folder: {entry.Key}:{entry.Value}");
-        }
-    }
-}
-System.Console.WriteLine($"Answer Part 2: {lowestFolder.ToString()}");
-
-// Suspend the screen.  
-System.Console.ReadLine();  
 
 // Recursive funtion that goes through the lines: summate the quanties and the returns
 (string, int) readDirectory(StreamReader sr, string directoryName){
@@ -77,3 +48,33 @@ System.Console.ReadLine();
     }
     return (directoryName,quantity);   
 }
+
+
+
+
+// **** PART 1 START ***** //
+foreach(KeyValuePair<string, int> entry in fileStructure){
+    if(entry.Value < 100000)
+        totalSum +=entry.Value;
+}
+System.Console.WriteLine($"Answer Part 1: {totalSum.ToString()}");
+// **** PART 1 END ***** //
+
+// **** PART 2 START ***** //
+int totalSpace = 70000000;
+int requiredSpace = 30000000;
+
+int lowestFolder = fileStructure["root//"];
+int remainingSpace = totalSpace - lowestFolder;
+foreach(KeyValuePair<string, int> entry in fileStructure){
+    if(remainingSpace + entry.Value >= requiredSpace)
+    {
+        if(entry.Value < lowestFolder)
+        {
+            lowestFolder = entry.Value;
+        }
+    }
+}
+// **** PART 2 END ***** //
+System.Console.WriteLine($"Answer Part 2: {lowestFolder.ToString()}")
+System.Console.ReadKey();  
